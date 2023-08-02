d39f7030b92127454cfcdcb835aaa4cddcb32cb7
David Morgan <haitheredavid@gmail.com>
2023-08-02 16:56:47 -0400

    basic model

---
+++ viewto-frontend/src/models/scenario.tsx
@@ -0,0 +1,21 @@
+
+class scenario {
+
+    project: string;
+    model: string;
+    version: string;
+    views: null;
+
+
+    // computed stuff
+    Points: null;
+    Focuses: null;
+    Conditions: null;
+
+    // volatile 
+    data: null;
+
+}
+
+
+export default scenario;
