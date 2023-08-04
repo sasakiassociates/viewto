45459472e35dd9bf923029d0f118ebef2d490e0f
Eric Youngberg <eyoungberg@sasaki.com>
2023-08-04 10:28:08 -0400

    ViewsStore

---
+++ viewto-frontend/src/stores/ViewsStore.ts
@@ -0,0 +1,27 @@
+import { stores, Store } from '@strategies/stores';
+import { action, computed, makeObservable, observable } from 'mobx';
+
+import Stores from './Stores';
+import { View } from '../models/View';
+
+
+export default class ViewsStore extends Store {
+
+    constructor() {
+        super();
+        makeObservable(this);
+    }
+
+    @computed
+    get all() {
+        return (stores as Stores).scenario.views;
+    }
+
+    @computed
+    get active(): View {
+        return this.all.filter(v => v.active)[0];
+    }
+    
+    virtual = new View({});
+   
+}
