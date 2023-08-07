ef8cb062df70f0a46867a09cb5977f1cd3cd84e8
David Morgan <haitheredavid@gmail.com>
2023-08-07 15:19:29 -0400

    project id default value

--- viewto-frontend/src/models/Project.ts
+++ viewto-frontend/src/models/Project.ts
@@ -3,7 +3,7 @@ import { Model, model, prop } from 'mobx-keystone';
 @model("viewto/Project")
 export class Project extends Model({
     // the id of the speckle project (stream)  
-    id: prop<string>(),
+    id: prop<string>(""),
     // the path to the speckle model(branch) 
     model: prop<string>(() => "main"),
     // the id to the speckle version(commit) 
