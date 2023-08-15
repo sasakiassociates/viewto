a3e71611915e2d1ae8ea43eb7cbaeac4c42b3f10
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-08-15 15:19:02 -0400

    duplicate from merge

--- viewto-frontend/src/models/Scenario.ts
+++ viewto-frontend/src/models/Scenario.ts
@@ -1,13 +1,9 @@
 import { Store } from '@strategies/stores';
-import { action, observable, reaction } from 'mobx';
-import { Store } from '@strategies/stores';
-import { action, observable, reaction } from 'mobx';
+import { reaction, observable, action } from 'mobx';
 import { Model, model, prop } from 'mobx-keystone';
 
 
 import { Project } from './Project';
-import ObjectLoader from "@speckle/objectloader";
-import { ViewStudy } from './ViewStudy';
 import ObjectLoader from "@speckle/objectloader";
 import { ViewStudy } from './ViewStudy';
 import { View } from './View';
@@ -16,28 +12,20 @@ import { View } from './View';
 export class Scenario extends Model({
     // the information linked to the speckle project loaded
     project: prop<Project>(() => new Project({})),
-    project: prop<Project>(() => new Project({})),
     // the list of views stored to apply the settings 
     views: prop<View[]>(() => []),
 }) implements Store {
-}) implements Store {
 
-    onRegister() { }
-    onUpdateRegistration() { }
     onRegister() { }
     onUpdateRegistration() { }
 
     @observable
     study: ViewStudy;
-    study: ViewStudy;
 
     @action
     setStudy(data: any) {
         this.study = data;
-    setStudy(data: any) {
-        this.study = data;
     }
-
     onInit() {
         reaction(
             () => [this.study?.id],
