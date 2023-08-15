6998a7e407c7db3294385f27cec45354dfd18055
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-08-15 12:54:57 -0400

    add dotenv

--- viewto-frontend/src/App.tsx
+++ viewto-frontend/src/App.tsx
@@ -1,21 +1,22 @@
-import MenuBar from './components/MenuBar/MenuBar';
-import LayersPanel from './components/LayersPanel/LayersPanel';
-import SelectionPanel from './components/SelectionPanel/SelectionPanel';
-import PointViewPanel from './components/PointViewPanel/PointViewPanel';
-import Viewer from './components/Viewer/Viewer';
-import { ObjectConverter } from './converter/ObjectConverter';
+import MenuBar from "./components/MenuBar/MenuBar";
+import LayersPanel from "./components/LayersPanel/LayersPanel";
+import SelectionPanel from "./components/SelectionPanel/SelectionPanel";
+import PointViewPanel from "./components/PointViewPanel/PointViewPanel";
+import Viewer from "./components/Viewer/Viewer";
 
 export default function App() {
-    const loader = ObjectConverter();
-    return (
-        <div className="ViewTo">
-            <div className="ViewTo__layout">
-                <MenuBar />
-                <LayersPanel />
-                <SelectionPanel />
-                <PointViewPanel />
-                <Viewer />
-            </div>
-        </div>
-    );
+  // @ts-ignore
+  console.log(import.meta.env.VITE_SPECKLE_TOKEN);
+
+  return (
+    <div className="ViewTo">
+      <div className="ViewTo__layout">
+        <MenuBar />
+        <LayersPanel />
+        <SelectionPanel />
+        <PointViewPanel />
+        <Viewer />
+      </div>
+    </div>
+  );
 }
