2b6323af995f5ae6d16eef960d9cd1fb6fe48ce3
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-08-15 14:47:54 -0400

    Squashed commit of the following:
    commit f26a0e9375be89d505259703ced4ea5a95405310
    Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
    Date:   Tue Aug 15 11:21:47 2023 -0400
    
        Squashed commit of the following:
    
        commit 404bfabbbadb0c4d270d761a579177e83eb60fda
        Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
        Date:   Mon Aug 14 18:05:38 2023 -0400
    
            Scenario and Object Mapping (#6)
    
        commit 7a12058e1da204761897325b4f17f06807d597e9
        Author: Eric Youngberg <eric@lmtlss.net>
        Date:   Fri Aug 4 13:14:52 2023 -0400
    
            271  - Panel layout (#5)
    
            * Cleanup
    
            * UIStore and registerStores function
    
            * Provide Scenario model with defaults for early dev.
    
            * PointsStore
    
            * ViewsStore
    
            * UserStore
    
            * Layout and Panels
    
        commit 5a0c79b1b782bb44926f7c7f73c5ce04eb7265e0
        Author: Eric Youngberg <eric@lmtlss.net>
        Date:   Fri Aug 4 11:57:12 2023 -0400
    
            Stores (#4)
    
            * Cleanup
    
            * UIStore and registerStores function
    
            * Provide Scenario model with defaults for early dev.
    
            * PointsStore
    
            * ViewsStore
    
            * UserStore
    
    commit 640bd6bc0dbc500c25e6f27a6195797421f3ca0b
    Merge: dc64309 a76dcf9
    Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
    Date:   Fri Aug 4 09:47:33 2023 -0400
    
        Merge branch 'main' into sp17
    
    commit dc6430988b3c29225cc8ae7624692cd09217d836
    Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
    Date:   Thu Aug 3 14:58:30 2023 -0400
    
        added prettier
    
    commit f99f4a2db22b246b3e38e377f5ff2310d0dbdc74
    Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
    Date:   Thu Aug 3 08:53:44 2023 -0400
    
        ts config setup with packages (#2)
    
        * ts config setup with packages
    
        * added missing dependencies

--- viewto-frontend/src/models/Scenario.ts
+++ viewto-frontend/src/models/Scenario.ts
@@ -1,8 +1,13 @@
 import { Store } from '@strategies/stores';
 import { action, observable, reaction } from 'mobx';
+import { Store } from '@strategies/stores';
+import { action, observable, reaction } from 'mobx';
 import { Model, model, prop } from 'mobx-keystone';
 
+
 import { Project } from './Project';
+import ObjectLoader from "@speckle/objectloader";
+import { ViewStudy } from './ViewStudy';
 import ObjectLoader from "@speckle/objectloader";
 import { ViewStudy } from './ViewStudy';
 import { View } from './View';
@@ -11,26 +16,29 @@ import { View } from './View';
 export class Scenario extends Model({
     // the information linked to the speckle project loaded
     project: prop<Project>(() => new Project({})),
+    project: prop<Project>(() => new Project({})),
     // the list of views stored to apply the settings 
     views: prop<View[]>(() => []),
 }) implements Store {
+}) implements Store {
 
+    onRegister() { }
+    onUpdateRegistration() { }
     onRegister() { }
     onUpdateRegistration() { }
 
     @observable
     study: ViewStudy;
+    study: ViewStudy;
 
     @action
+    setStudy(data: any) {
+        this.study = data;
     setStudy(data: any) {
         this.study = data;
     }
 
     onInit() {
-        if (this.project) {
-            this._loadStudy(study => this.setStudy(study));
-        }
-
         reaction(
             () => [this.study?.id],
             () => {
@@ -45,67 +53,7 @@ export class Scenario extends Model({
             }
         );
     }
-
-
-
-    private _loadStudy(fn: (data: ViewStudy) => void) {
-        console.log(`Loading new Project: ${this.project.id}`);
-
-        const viewStudyExampleReference = "f4b16ebe7e93ea3ec653cd284d72ca05";
-
-        const loader = new ObjectLoader({
-            // @ts-ignore
-            token: import.meta.env.SPECKLE_TOKEN,
-            // @ts-ignore
-            serverUrl: import.meta.env.SPECKLE_URL,
-            streamId: this.project.id,
-            objectId: this.project.version,
-            // options: {
-            //     fullyTraverseArrays: false, // Default: false. By default, if an array starts with a primitive type, it will not be traversed. Set it to true if you want to capture scenarios in which lists can have intersped objects and primitives, e.g. [ 1, 2, "a", { important object } ]
-            //     excludeProps: ["displayValue", "displayMesh", "__closure"], // Default: []. Any prop names that you pass in here will be ignored from object construction traversal.
-            // },
-        });
-
-        const getObjectAndManuallyConvert = async (): Promise<void> => {
-
-            const referenceObj = await loader.getAndConstructObject((e) => {
-                // event loop for getting progress on the loading
-                console.log("Progress ", e.stage, ":", e.current / e.total);
-            });
-
-            // deconstructing the speckle object
-            // @ts-ignore
-            fn(new ViewStudy(referenceObj.Data));
-        };
-
-        getObjectAndManuallyConvert();
-    }
-
-    private _loadStudyModel() {
-        console.log(`Loading new Study: ${this.study.name}`);
-
-        // we get every mesh from the reference objects and load them into the scene 
-        this.study.getSpeckleMeshes.map(reference => {
-            const loader = new ObjectLoader({
-                // @ts-ignore
-                token: import.meta.env.SPECKLE_TOKEN,
-                // @ts-ignore
-                serverUrl: import.meta.env.SPECKLE_URL,
-                streamId: this.project.id,
-                objectId: reference
-            });
-
-            const getObjectAndManuallyConvert = async (): Promise<void> => {
-
-                const referenceObj = await loader.getAndConstructObject((e) => {
-                    // event loop for getting progress on the loading
-                    console.log("Progress ", e.stage, ":", e.current / e.total);
-                });
-
-                // this is now just rendering data that we want in the viewer
-                // return referenceObj.Data 
-            };
-        })
-    }
-
 }
+
+
+
