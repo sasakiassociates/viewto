650671483e2d53f657677f0faa4c9babd1b388e1
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-08-16 09:45:42 -0400

    305-Missing pieces from merge  (#9)
    * Stores (#4)
    
    * Cleanup
    
    * UIStore and registerStores function
    
    * Provide Scenario model with defaults for early dev.
    
    * PointsStore
    
    * ViewsStore
    
    * UserStore
    
    * 271  - Panel layout (#5)
    
    * Cleanup
    
    * UIStore and registerStores function
    
    * Provide Scenario model with defaults for early dev.
    
    * PointsStore
    
    * ViewsStore
    
    * UserStore
    
    * Layout and Panels
    
    * working prototype
    
    * additional models for keystone
    
    * updated scenario with new models
    
    * refactored models
    
    * added points
    
    * updated scenario mapping
    
    * Create .prettierrc
    
    * Squashed commit of the following:
    
    commit f26a0e9375be89d505259703ced4ea5a95405310
    Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
    Date:   Tue Aug 15 11:21:47 2023 -0400
    
        Squashed commit of the following:
    
        commit 404bfabbbadb0c4d270d761a579177e83eb60fda
        Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
        Date:   Mon Aug 14 18:05:38 2023 -0400
    
            Scenario and Object Mapping (#6)
    
        commit 7a12058e1da204761897325b4f17f06807d597e9
        Author: Eric Youngberg <eric@lmtlss.net>
        Date:   Fri Aug 4 13:14:52 2023 -0400
    
            271  - Panel layout (#5)
    
            * Cleanup
    
            * UIStore and registerStores function
    
            * Provide Scenario model with defaults for early dev.
    
            * PointsStore
    
            * ViewsStore
    
            * UserStore
    
            * Layout and Panels
    
        commit 5a0c79b1b782bb44926f7c7f73c5ce04eb7265e0
        Author: Eric Youngberg <eric@lmtlss.net>
        Date:   Fri Aug 4 11:57:12 2023 -0400
    
            Stores (#4)
    
            * Cleanup
    
            * UIStore and registerStores function
    
            * Provide Scenario model with defaults for early dev.
    
            * PointsStore
    
            * ViewsStore
    
            * UserStore
    
    commit 640bd6bc0dbc500c25e6f27a6195797421f3ca0b
    Merge: dc64309 a76dcf9
    Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
    Date:   Fri Aug 4 09:47:33 2023 -0400
    
        Merge branch 'main' into sp17
    
    commit dc6430988b3c29225cc8ae7624692cd09217d836
    Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
    Date:   Thu Aug 3 14:58:30 2023 -0400
    
        added prettier
    
    commit f99f4a2db22b246b3e38e377f5ff2310d0dbdc74
    Author: David Morgan <30870056+haitheredavid@users.noreply.github.com>
    Date:   Thu Aug 3 08:53:44 2023 -0400
    
        ts config setup with packages (#2)
    
        * ts config setup with packages
    
        * added missing dependencies
    
    * duplicate from merge
    
    * missing pieces
    
    * Delete ObjectConverter.ts
    
    * added dotenv and loader
    
    ---------
    
    Co-authored-by: Eric Youngberg <eric@lmtlss.net>

--- viewto-frontend/src/models/Scenario.ts
+++ viewto-frontend/src/models/Scenario.ts
@@ -41,6 +41,59 @@ export class Scenario extends Model({
             }
         );
     }
+
+
+    private _loadStudy(fn: (data: ViewStudy) => void) {
+        console.log(`Loading new Project: ${this.project.id}`);
+
+        const viewStudyExampleReference = "f4b16ebe7e93ea3ec653cd284d72ca05";
+
+        const loader = new ObjectLoader({
+            // @ts-ignore
+            token: import.meta.env.SPECKLE_TOKEN,
+            // @ts-ignore
+            serverUrl: import.meta.env.SPECKLE_URL,
+            streamId: this.project.id,
+            objectId: this.project.version,
+            // options: {
+            //     fullyTraverseArrays: false, // Default: false. By default, if an array starts with a primitive type, it will not be traversed. Set it to true if you want to capture scenarios in which lists can have intersped objects and primitives, e.g. [ 1, 2, "a", { important object } ]
+            //     excludeProps: ["displayValue", "displayMesh", "__closure"], // Default: []. Any prop names that you pass in here will be ignored from object construction traversal.
+        })
+
+        const referenceObj = await loader.getAndConstructObject((e) => {
+            // event loop for getting progress on the loading
+            console.log("Progress ", e.stage, ":", e.current / e.total);
+        });
+
+        // @ts-ignore
+        fn(new ViewStudy(referenceObj.Data))
+    };
+
+    private _loadStudyModel() {
+        console.log(`Loading new View Study: ${this.study.name}`);
+
+        // deconstructing the speckle object
+        // we get every mesh from the reference objects and load them into the scene 
+        this.study.getSpeckleMeshes.map(reference => {
+            const loader = new ObjectLoader({
+                // @ts-ignore
+                token: import.meta.env.SPECKLE_TOKEN,
+                // @ts-ignore
+                serverUrl: import.meta.env.SPECKLE_URL,
+                streamId: this.project.id,
+                objectId: reference
+            });
+
+            const referenceObj = await loader.getAndConstructObject((e) => {
+                // event loop for getting progress on the loading
+                console.log("Progress ", e.stage, ":", e.current / e.total);
+            });
+
+            // this is now just rendering data that we want in the viewer
+            // @ts-ignore
+            return referenceObj.Data 
+        }
+    };
 }
 
 
