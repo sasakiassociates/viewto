60b8208d86bbf6b63a54cf9ed49746e19f250da9
arminakvn <armin.akhavan@gmail.com>
2023-08-22 10:55:57 -0400

    base impl ...

--- viewto-frontend/src/components/MenuBar/MenuBar.tsx
+++ viewto-frontend/src/components/MenuBar/MenuBar.tsx
@@ -1,12 +1,54 @@
 import { observer } from 'mobx-react';
-import { MenuBar, Title, Body } from '@strategies/ui';
+import { MenuBar, Title, Body, IconButton } from '@strategies/ui';
+import { FiFolder, FiHelpCircle, FiSave, FiSettings, FiShare } from 'react-icons/fi'
+import { useStores } from '@strategies/stores';
+import { RadioSlider } from "@strategies/react-form";
+
+import Stores from '../../stores/Stores';
+import { UIMode } from '../../stores/UIStore';
+
 
 
 export default observer(function ViewToMenuBar() {
+    const { scenario, ui } = useStores<Stores>()
+
+
     return (
-        <MenuBar drawer={false}>
+        <MenuBar drawer={true}>
             <Title>View Analysis Dashboard</Title>
             <Body>
+                <div className='menu-icons' >
+                    <IconButton>
+                        <FiHelpCircle />
+                    </IconButton>
+                    <IconButton>
+                        <FiSettings />
+                    </IconButton>
+                    <IconButton>
+                        <FiShare />
+                    </IconButton>
+                    <IconButton>
+                        <FiFolder />
+                    </IconButton>
+                </div>
+                <div className="scenario-name-wrapper">
+                    <div className="scenario-name">
+                        <label htmlFor="scenario">Name</label>
+                        <input id="scenario" placeholder="Name ..." value={scenario.study?.name}
+                        // onChange={} 
+                        />
+                    </div>
+                </div>
+                <div className='menu-icons' >
+                    <IconButton>
+                        <FiSave />
+                    </IconButton>
+                </div>
+                <RadioSlider
+                    value={ui.mode}
+                    options={[UIMode.COMPARE, UIMode.EXPLORE]}
+                    onChange={value => ui.setMode(value as UIMode)}
+                />
             </Body>
         </MenuBar>
     );
