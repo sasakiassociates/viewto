e82d7af9a26de9783dc3d1f277a22f0297d850c8
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2023-08-23 12:42:08 -0400

    Loading to Viewer (#14)
    * removed viewer loading from scenario
    
    * added viewer
    
    * brought back reference method for study
    
    * added computed property for setting the ref obj
    
    * implemented viewer
    
    * modified reference obj prop
    
    * switched reference to computed

--- viewto-frontend/src/models/Scenario.ts
+++ viewto-frontend/src/models/Scenario.ts
@@ -35,7 +35,7 @@ export class Scenario extends Model({
 
     onInit() {
         if (this.project && this.project.complete) {
-            (async() => {
+            (async () => {
                 this.setStudy(await this._loadStudyFromProject());
             })();
         }
@@ -65,14 +65,6 @@ export class Scenario extends Model({
         // not really necessary, but this is a simple way to make sure we have an authenticated
         console.log(await this.speckle.activeUser);
 
-        /* 
-         the scenario model references the speckle project(stream) that we need to pull the referene object from the database
-         the ui will give us the input for this project and object
-         for now we have a hard coded test project in place
-        */
-
-        // console.log(`Loading new Project: ${this.project.name}\n${this.project.id}\n${this.project.model}\n${this.project.version}`);
-
         // load the version (commit) data
         const version = await this.speckle.Project(import.meta.env.VITE_VIEWTO_TEST_PROJECT).Version(import.meta.env.VITE_VIEWTO_TEST_VERSION).get;
         console.log(version);
@@ -83,34 +75,8 @@ export class Scenario extends Model({
 
         // deconstruct all the view study data
         // @ts-ignore
-        const study = new ViewStudy(referenceObj.Data);
-        return study;
+        return new ViewStudy(referenceObj.Data);
     };
-
-    /*
-    private async _loadStudyReferences(fn: (reference: string) => void) {
-        console.log(`Loading new View Study: ${this.study?.name}`);
-
-        const loadVersion = async (reference: string, type: string) => {
-            console.log(`Loading new ${type} : ${reference}`);
-            const obj = await this.speckle.Project(import.meta.env.VITE_VIEWTO_TEST_PROJECT).Version(reference).get;
-            //TODO: this is where we laod the viewer data into speckle
-
-        }
-
-
-        // 1. load in all of the geometry references
-        // we get every mesh from the reference objects and load them into the scene 
-        this.study.getContextReferences.map(ref => loadVersion(ref, "tbd"));
-        this.study.getCloudReferences.map(cld => loadVersion(cld, "tbd"));
-
-        // 2. load the point cloud
-        // 3. apply the results
-
-        // 4. end 
-
-    };
-    */
 }
 
 
