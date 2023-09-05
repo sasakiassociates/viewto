33ee0fc4ae93b6dbd03f1f9dc475d89c8eb80be6
David Morgan <haitheredavid@gmail.com>
2023-09-05 12:04:53 -0400

    fix for colorizing

--- viewto-frontend/src/util.ts
+++ viewto-frontend/src/util.ts
@@ -1,3 +1,33 @@
 export type Dict<T = any> = {[key:string]: T};
 
 export function noop() {}
+
+/* missing stuff from dmo */
+export function getMinMax(array: number[]): number[] {
+    let min = Number.MAX_SAFE_INTEGER;
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) min = array[i];
+        else if (array[i] > max) max = array[i];
+    }
+    return [min, max];
+}
+
+// normalise values
+export function normalise(array: number[], min: number, max: number): number[] {
+    let results = [];
+    for (let i = 0; i < array.length; i++) {
+        results[i] = (array[i] - min) / (max - min);
+    }
+    return results;
+}
+
+export function clamp(array: number[], min: number, max: number): number[] {
+    let results = [...array];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) results[i] = min;
+        else if (array[i] > max) results[i] = max;
+    }
+    return results;
+}
